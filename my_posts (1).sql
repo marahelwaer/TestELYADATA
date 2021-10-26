a739f6e6394174e0721a641c519d4e805b3f6dc3
MarahElwaer <55142588+marahelwaer@users.noreply.github.com>
2021-10-27 00:41:00 +0100

    Add files via upload

---
+++ my_posts (1).sql
@@ -0,0 +1,16 @@
+CREATE TABLE mytable(
+   FIELD1         INTEGER  NOT NULL PRIMARY KEY 
+  ,published      VARCHAR(6) NOT NULL
+  ,description    VARCHAR(253) NOT NULL
+  ,images         VARCHAR(2) NOT NULL
+  ,post_url       VARCHAR(30)
+  ,external_links VARCHAR(2) NOT NULL
+  ,like_url       VARCHAR(30)
+);
+INSERT INTO mytable(FIELD1,published,description,images,post_url,external_links,like_url) VALUES (0,'2 min','Heavy rain and wind hit the New York area as an early season Nor’easter pounded the region. A flash flood watch is in effect, and strong winds of 40 to 60 miles per hour are expected into Wednesday.','[]',NULL,'[]',NULL);
+INSERT INTO mytable(FIELD1,published,description,images,post_url,external_links,like_url) VALUES (1,'8 min','Breaking News: The latest national pledges to curb emissions would produce just one-seventh of the cuts needed to avoid dangerous warming, a UN report warned.
+ “The clock is ticking loudly,” the UN said.','[]',NULL,'[]',NULL);
+INSERT INTO mytable(FIELD1,published,description,images,post_url,external_links,like_url) VALUES (2,'22 min','U.S. lawmakers on Tuesday are set to grill executives from YouTube, Snap and TikTok about mounting concerns that their platforms can harm children and teenagers.','[]',NULL,'[]',NULL);
+INSERT INTO mytable(FIELD1,published,description,images,post_url,external_links,like_url) VALUES (3,'37 min','An FDA advisory panel is set to consider whether to recommend Pfizer’s Covid vaccine for 5- to 11-year-olds. The panel’s recommendation would set in motion a string of decisions that could lead to children getting shots as early as the end of next week.','[]',NULL,'[]',NULL);
+INSERT INTO mytable(FIELD1,published,description,images,post_url,external_links,like_url) VALUES (4,'52 min','With Democrats on the brink of reaching a deal to expand the social safety net in the U.S., the cuts and compromises they’ve made could complicate their efforts.
+ Listen to today’s episode of The Daily.','[]',NULL,'[]',NULL);
